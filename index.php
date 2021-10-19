2125a638a52f63600c63ac18a162a2bbc9756f1e
Sebi Coroian <escapemcro@gmail.com>
2021-10-19 20:18:44 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+   
+    <title>Sebastian Coroian - Digital Media Marketing</title>
+ 
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+
+	<nav class="navbar navbar-expand-lg bg-secondary fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand" href="#page-top">Sebastian Coroian</a>
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link py-3 px-3 rounded" href="#portfolio">Portfolio</a></li>
+                        <li class="nav-item"><a class="nav-link py-3 px-3 rounded" href="#about">About</a></li>
+                        <li class="nav-item"><a class="nav-link py-3 px-3 rounded" href="#contact">Contact</a></li>
+                    </ul>
+            </div>
+     </nav>
+
+
+        <header class="masthead bg-primary text-white text-center">
+            <div class="container align-items-center">
+                
+
+                <img class= "masthead-avatar mb-5" style="border-radius: 20%;" src="assets/img/me.jpg"/>
+                
+                <h1 class="masthead-heading">Is it still possible to CRUSH your ROI via paid advertising?</h1>
+                
+                <div class="divider-custom divider-light mb-5 mt-5">
+                    <div class="divider-custom-line"></div>
+                </div>
+
+                <p class="masthead-subheading mb-5">It's time to stop throwing your money away in the social media dumpster</p>
+
+            	<div class="text mb-5">
+      			<h6>
+        			Running intelligent advertising campaigns is far from rocket science. However, there are certain principles that you need to know previous to applying them in your favor. For paid social media traffic, these are:
+				</h6>
+				
+				<ul class="text">
+					<li>1. Know Your Audience</li>
+					<li>2. Recognize & Use The Human Behavior Triggers</li>
+					<li>3. Master The Platform </li>
+				</ul>
+
+				<h6 class="text mb-0">
+        			Bend these together and you're left with a lean, clean and efficient framework that gets you client after client on autopilot.
+				</h6>
+
+				<h6 class="text mb-0">
+					If you are an eCommerce store owner and you want to take your sales to the next level, or you already get sales and want to outsource and refine your system, it is time to work together. 
+				</h6>
+				</div>
+           
+            </div>
+        </header>
+
+        <section class="page-section portfolio" id="portfolio">
+           
+            <div class="container ">
+               <h2 class="page-section-heading text-center text-secondary mb-5">PORTOFOLIO</h2>
+
+                <div class="divider-custom mb-5">
+                    <div class="divider-custom-line"></div>
+                </div>
+        
+                <?php 
+                  $smartpushURL = "https://www.youtube.com/watch?v=Z8gcMoNc6QM";
+                  $convertedSmartpushURL = str_replace("watch?v=","embed/", $smartpushURL);
+                ?>              
+
+                <?php 
+                  $smartplugURL = "https://www.youtube.com/watch?v=g9vtiZalueM";
+                  $convertedSmartplugURL = str_replace("watch?v=","embed/", $smartplugURL);
+                ?>   
+
+                <?php 
+                  $vectronURL = "https://www.youtube.com/watch?v=aY7Iqax1RPI";
+                  $convertedVectronURL = str_replace("watch?v=","embed/", $vectronURL);
+                ?>   
+
+                <?php 
+                  $cvURL = "https://www.youtube.com/watch?v=rQeKM_BS_P4";
+                  $convertedCvURL = str_replace("watch?v=","embed/", $cvURL);
+                ?>   
+               
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-sm">
+                          <iframe class="embed-responsive-item" src="<?php echo $convertedSmartpushURL; ?>"></iframe>
+                        </div>
+                        <div class="col-sm">
+                          <iframe class="embed-responsive-item" src="<?php echo $convertedVectronURL; ?>"></iframe>
+                        </div>
+                        <div class="col-sm">
+                         <iframe class="embed-responsive-item" src="<?php echo $convertedSmartplugURL; ?>"></iframe>
+                        </div>
+                      </div>
+                    </div>
+               
+                </div>
+                
+                <p><br><br><br><br><br><br></p>
+
+        <section class="page-section bg-primary text-white mb-0" id="about">
+
+            <div class="container">
+                <h2 class="page-section-heading text-center text-white">ABOUT</h2>
+
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                </div>
+
+                <div class="text-white">
+                    <h6 class="text-center">Hi! My name is Sebastian, I'm 21, currently a senior business student.</h6></div>
+                    <p class="text-center">In order to give you, as an employer the best picture of myself, taking a couple of lines to tell you about my personal life feels mandatory.</p>
+                    <p class="text-center">
+                    Whenever asked, all of my friends describe me as a life lover. I couldn't agree more on this. I'm the kind of person that enjoys the little things and lives the moment.
+                    Life is too short not to seek stories to tell. Looking forward to collecting as many experiences and good memories as I can possibly handle. Always an enthusiast of adventures and hands-on activities.
+                    </p>
+                    <p class="text-center">A thing I can list without a doubt is my extroverted nature. Love meeting and spending quality time with quality people.
+                    Some of my hobbies include skateboarding, fitness, cycling, image editing, basketball and hikes.</p>
+                    <p class="text-center">At this point in my life, I am pretty happy with where I am and what I have achieved.
+                    What I'm looking for right now is a place to fit within a team of nice individuals, solving tasks that get your mind going.</p>
+                    <p class="text-center">I'm listing down below a couple of projects that had a major impact on my persona:</p>
+
+                <div class="row">
+                    <div class="col-lg-4 ms-auto"><h6>FIRST Robotics</h6><p class="lead">FIRST Tech Challenge is an American program meant to inspire teenagers STEM concepts. My main responsibility was programming the sensors and motors of our robot using their native Java frameworks so it could perform autonomous tasks (on its own) and performing tasks while remotely controlled by a driver.</p><p class="lead">As the leader of this department (that consisted of 3 people) I was also responsible for setting deadlines for my team, training the newcomers, giving feedback on the code and summarizing our daily activity in a team journal called "Engineering Notebook".</p><p class="lead">Unrelated to my role in the team, due to my love for Photoshop, I had the opportunity to create posters and digital marketing material for the media department.</p></div>
+                   
+                    <div class="col-lg-4 me-auto"><h6>DPIT</h6><p class="lead">DPIT (Descopera-ti Pasiunea in IT) is a mentorship program for high-school students. The students are meant to develop a functional software product to the point it can be launched on the market within 8 months under the guidance of experienced mentors.</p><p class="lead">I took part in this program for 2 seasons in which my team focused on IoT (Internet of Things) sided products. In these seasons out products consisted of a smart button/switch pusher that could be actioned from anywhere around the world and a smart wall outlet that could also be actioned remotely using a mobile app.</p><p class="lead">The established company mentoring me was Around25. I met weekly with my mentor so we could set tasks, deadlines, brainstorm ideas and look over the issues in my code. In every season, for 2 weeks I would go daily to the company from 9 to 5 such as a regular employee working on my project.</p><p class="lead">Despite the mentoring, this program hosted various workshops and courses I attended in matters of: OOP, Data Bases, teamwork, marketing, public speaking, entrepreneurship and many others. </p></div>
+                </div>
+        </div>
+        </section>
+
+        <div class="row justify-content-center" id="contact">
+        <div class="calendly-inline-widget" data-url="https://calendly.com/sebastiancoroian2000/15min?hide_gdpr_banner=1" style="min-width:320px;height:630px;"></div>
+        <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+        </div>
+</body>
+
+        <footer class="footer text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col mb-5">
+                        <h4 class="text-uppercase mb-4">Social Media</h4>
+                        <a class="text text-white" href="https://www.facebook.com/sebi.coroian.9/">FACEBOOK</a>
+                        <a class="text text-white" href="https://github.com/SebiCoroian">GITHUB</a>
+                        <a class="text text-white" href="https://www.linkedin.com/in/sebastian-coroian-a94587221/">LINKEDIN</a>      
+                    </div>
+                    <div class="col">
+                        <p>sebastiancoroian2000@gmail.com</p>
+                        <h4>Let's get your business to the next level today!</h4>
+                    </div>
+                    <div class="col">
+                    <div class="container"> 
+
+                    <script>
+                        function generateRandomNumber() {
+                        var min = parseInt(document.getElementById('min').value);
+                        var max = parseInt(document.getElementById('max').value);
+                        var rand = Math.floor(Math.random()* (max - min + 1)) + min;
+                        document.getElementById('display').innerText = rand }
+                    </script>  
+      
+                    <h4>Random Number Generator</h4>
+                        <div class="input-group">
+                            <input id="min" type="number" class="text-center form-control" placeholder="Min Number">
+                        </div>
+
+                        <div class="input-group">
+                            <input id="max" type="number" class="text-center form-control"  placeholder="Max Number">
+                        </div>
+      
+                        <button class="btn btn-primary" onclick="generateRandomNumber();"> Generate</button>
+
+                        <div id="display">   
+                        </div>
+                    </div>
+                    </div>
+                </div>
+        </footer>
+
+</html>
